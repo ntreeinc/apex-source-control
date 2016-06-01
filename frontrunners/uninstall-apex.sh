a44f6d2356dcba08c8fd3a351a2595cae5295a71
nathan <nathan@ntree.com>
2016-06-01 13:42:54 -0400

    uninstall-apex frontrunner script fixed

--- frontrunners/uninstall-apex.sh
+++ frontrunners/uninstall-apex.sh
@@ -1,4 +1,4 @@
 # designed to be called from top level project dir
 node_modules/apex-source-control/scripts/check_conf_file.sh
 source config/apexupdate.conf 
-sqlplus $username/$password@$database_connection @../node_modules/apex-source-control/scripts/uninstall_apex.sql $apexappid $workspace_name $parsing_schema
+sqlplus $username/$password@$database_connection @node_modules/apex-source-control/scripts/uninstall_apex.sql $apexappid $workspace_name $parsing_schema
