54c5ee8b74845680d04e6f47eba6061b6f354dcb
nathan <nathan@ntree.com>
2016-06-01 13:30:48 -0400

    generate new app id fixed output error

--- scripts/generate_new_app_id.sql
+++ scripts/generate_new_app_id.sql
@@ -3,7 +3,6 @@ set serveroutput on
 set feedback off
 begin
    apex_application_install.generate_application_id;
-   dbms_output.put_Line("Your new apex app id is..."); --This line should be commented out if you want to use spooling
    dbms_output.put_Line(apex_application_install.get_application_id);
 end;
 /
