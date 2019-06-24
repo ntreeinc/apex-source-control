3b11f3d5f1d774c6a8697e0276db183fdee25df5
Michael Buckley <michael@ntree.com>
2019-06-24 17:05:31 -0400

    Delete dead scripts

--- scripts/generate_new_app_id.sql
+++
@@ -1,8 +0,0 @@
-set serveroutput on
-set feedback off
-begin
-   apex_application_install.generate_application_id;
-   dbms_output.put_Line(apex_application_install.get_application_id);
-end;
-/
-exit
